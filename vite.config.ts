56ef920304919e0b67a5dd5a066d5f374894c69b
Mahefa-LANDRIE <landriejames@gmail.com>
2026-05-27 13:07:13 +0300

    Manampy allowedHosts ho an'ny DigitalOcean

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   vite: {
     base: '/',
+    preview: {
+      allowedHosts: ['landrieportfolio-app-9uxlt.ondigitalocean.app']
+    }
   },
   tanstackStart: {
     server: { entry: "server" },
