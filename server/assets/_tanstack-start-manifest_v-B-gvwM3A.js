0b9d1df210ded92bd65b2f19fcfd5da04e709abb
Mahefa-LANDRIE <Mahefa-LANDRIE@users.noreply.github.com>
2026-05-27 08:43:57 +0000

    deploy: 6ea55762acc7a8db74a9c666feaca65fc3df0b67

--- server/assets/_tanstack-start-manifest_v-B-gvwM3A.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/home/runner/work/portfolio_final/portfolio_final/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-0kEMncEx.js"] }, "/": { filePath: "/home/runner/work/portfolio_final/portfolio_final/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-Dvj5ru3A.js"] } }, clientEntry: "/assets/index-0kEMncEx.js" });
-export {
-  tsrStartManifest
-};
